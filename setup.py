1ea5ee1e2e706dce1a154e1b3c11bdeea0564c2d
Bishal Budhathoki <deverbishal331@gmail.com>
2021-07-15 03:49:55 +1000

    Initial Push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ATG',
+    version='1.6',
+    packages=[''],
+    url='',
+    license='',
+    author='Er.Bishal Budhathoki',
+    author_email='bishalkc331@gmail.com',
+    description=''
+)
